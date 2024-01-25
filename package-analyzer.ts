dba3a3ee05faa3bcebb48ec56594bcd2904412a9
Hyojeong Choi <gywjddl001@naver.com>
2024-01-25 19:20:54 +0900

    ✨ Implement cli options

--- package-analyzer.ts
+++ package-analyzer.ts
@@ -1,6 +1,29 @@
-const cmd = new Deno.Command("du", {
-  args: ["-hd", "1", "node_modules/.pnpm"],
-});
-const { stdout } = await cmd.output();
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 
-console.log(stdout);
+const command = new Command();
+
+const run = async ({ path }: { path: string }) => {
+  const cmd = new Deno.Command("du", {
+    args: ["-hd", "1", path],
+    stdout: "piped",
+  });
+  const { stdout } = await cmd.output();
+  const output = new TextDecoder().decode(stdout);
+  const result = output.replaceAll(/^\s*[0-9.GKM]*\s*/gm, "");
+  console.log(result.split('\n').sort());
+};
+
+command
+  .name("package-analyzer")
+  .description("NodeJS package analyzer")
+  .option(
+    "-i, --input <file-path>",
+    "path to a node_modules or .pnpm directory",
+    {
+      default: "node_modules/.pnpm",
+    },
+  )
+  .action(({ input }) => {
+    run({ path: input });
+  })
+  .parse();
