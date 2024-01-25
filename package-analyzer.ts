eec7c99b70606900b352068eac6ee8640dca137a
Hyojeong Choi <gywjddl001@naver.com>
2024-01-25 20:08:25 +0900

    🚧 Wip: size print option

--- package-analyzer.ts
+++ package-analyzer.ts
@@ -4,26 +4,53 @@ import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 
 const command = new Command();
 
-const isPnpmLockFile = (a: unknown): a is {packages: Record<string, unknown>} => {
+const isPnpmLockFile = (
+  a: unknown,
+): a is { packages: Record<string, unknown> } => {
   // deno-lint-ignore no-explicit-any
-  if (typeof a === 'object' && typeof (a as any).packages === 'object') return true;
-   return false;
-}
+  if (typeof a === "object" && typeof (a as any).packages === "object") {
+    return true;
+  }
+  return false;
+};
 
 type RunArgs = {
   path: string;
   onlyDuplicated: boolean;
-}
-const run = async ({ path, onlyDuplicated }: RunArgs) => {
+  showFileSize: boolean;
+};
+
+// FIXME: pipe 형식으로 하도록 고쳐야지
+const run = async ({ path, onlyDuplicated, showFileSize }: RunArgs) => {
   const pnpmLockFileBody = await fs.readFile(path, { encoding: "utf8" });
   const parsed = parseYaml(pnpmLockFileBody);
+  if (showFileSize) {
+    const cmd = new Deno.Command("du", {
+      args: ["-hd", "1", "node_modules/.pnpm"],
+      stdout: "piped",
+    });
+    const { stdout } = await cmd.output();
+    const output = new TextDecoder().decode(stdout);
+    const sizeMap = new Map<string, string>();
+    output.split("\n").forEach((info) =>
+      {
+        const matchResult = info.match(/^\s*([0-9.GKM]*)\s*node_modules\/\.pnpm\/(.*)$/);
+        if (matchResult) {
+          const [size, packageName] = matchResult;
+          sizeMap.set(packageName, size);
+        }
+      }
+    );
+    console.log(sizeMap);
+    return;
+  }
   if (!isPnpmLockFile(parsed)) {
     console.error(`${path} is not a pnpm lock file`);
     Deno.exit(1);
   }
   const packages = Object.keys(parsed.packages).sort();
   if (!onlyDuplicated) {
-    packages.forEach(packageName => console.log(packageName));
+    packages.forEach((packageName) => console.log(packageName));
     Deno.exit(0);
   }
   const result = new Set<string>();
@@ -38,16 +65,6 @@ const run = async ({ path, onlyDuplicated }: RunArgs) => {
     }
   }
   result.forEach((packageName) => console.log(packageName));
-
-  // NOTE: pnpm 폴더 용량 계산하는 버전
-  // const cmd = new Deno.Command("du", {
-  //   args: ["-hd", "1", path],
-  //   stdout: "piped",
-  // });
-  // const { stdout } = await cmd.output();
-  // const output = new TextDecoder().decode(stdout);
-  // const result = output.replaceAll(/^\s*[0-9.GKM]*\s*/gm, "");
-  // console.log(result.split('\n').sort());
 };
 
 command
@@ -62,7 +79,11 @@ command
     "-d, --duplicated",
     "show only duplicated packages",
   )
-  .action(({ input, duplicated = false }) => {
-    run({ path: input, onlyDuplicated: duplicated });
+  .option(
+    "-s, --size",
+    "show file size per package from store",
+  )
+  .action(({ input, duplicated = false, size = false }) => {
+    run({ path: input, onlyDuplicated: duplicated, showFileSize: size });
   })
   .parse();
