7c06ccfa1e9200762917c246d9397850f67c0cc0
Hyojeong Choi <gywjddl001@naver.com>
2024-01-25 20:41:08 +0900

    ✨ Implement file size getter

--- package-analyzer.ts
+++ package-analyzer.ts
@@ -1,4 +1,3 @@
-import fs from "https://deno.land/std@0.173.0/node/fs/promises.ts";
 import { parse as parseYaml } from "https://deno.land/std@0.173.0/encoding/yaml.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 
@@ -20,51 +19,77 @@ type RunArgs = {
   showFileSize: boolean;
 };
 
-// FIXME: pipe 형식으로 하도록 고쳐야지
-const run = async ({ path, onlyDuplicated, showFileSize }: RunArgs) => {
-  const pnpmLockFileBody = await fs.readFile(path, { encoding: "utf8" });
-  const parsed = parseYaml(pnpmLockFileBody);
-  if (showFileSize) {
+class Runner {
+  packageNames: string[];
+  fileSizeMap = new Map<string, string>();
+
+  constructor(public path: string) {
+    const stream = Deno.readFileSync(path);
+    const pnpmLockFileBody = new TextDecoder().decode(stream);
+    const parsed = parseYaml(pnpmLockFileBody);
+    if (!isPnpmLockFile(parsed)) {
+      console.error(`${path} is not a pnpm lock file`);
+      Deno.exit(1);
+    }
+    this.packageNames = Object.keys(parsed.packages).map(packageName => packageName.slice(1)).sort();
+  }
+
+  filterDuplicated() {
+    const result = new Set<string>();
+    for (let i = 0; i < this.packageNames.length - 1; i += 1) {
+      const cur = this.packageNames[i];
+      const next = this.packageNames[i + 1];
+      const curPackageName = cur.split("@")[cur.startsWith("@") ? 1 : 0];
+      const nextPackageName = next.split("@")[next.startsWith("@") ? 1 : 0];
+      if (curPackageName === nextPackageName) {
+        result.add(cur);
+        result.add(next);
+      }
+    }
+    this.packageNames = Array.from(result);
+    return this;
+  }
+
+  getFileSizes() {
     const cmd = new Deno.Command("du", {
       args: ["-hd", "1", "node_modules/.pnpm"],
       stdout: "piped",
     });
-    const { stdout } = await cmd.output();
+    // FIXME: @mui/base@5.0.0-beta.24(@types/react@18.2.48)(react-dom@18.2.0)(react@18.2.0) 이런 케이스
+    // 때문에 파일쪽이랑 lock쪽 둘다 뭐 해줘야할지도
+    const { stdout } = cmd.outputSync();
     const output = new TextDecoder().decode(stdout);
-    const sizeMap = new Map<string, string>();
-    output.split("\n").forEach((info) =>
-      {
-        const matchResult = info.match(/^\s*([0-9.GKM]*)\s*node_modules\/\.pnpm\/(.*)$/);
-        if (matchResult) {
-          const [size, packageName] = matchResult;
-          sizeMap.set(packageName, size);
-        }
+    output.split("\n").forEach((info) => {
+      const matchResult = info.match(
+        /^\s*([0-9.GKM]*)\s*node_modules\/\.pnpm\/(.*@{1}[0-9.]*)$/,
+        // /^\s*([0-9.GKM]*)\s*node_modules\/\.pnpm\/(.*)$/,
+      );
+      if (matchResult) {
+        const [_, size, packageName] = matchResult;
+        this.fileSizeMap.set(packageName.replace('+', '/'), size);
       }
-    );
-    console.log(sizeMap);
-    return;
+    });
+    return this;
   }
-  if (!isPnpmLockFile(parsed)) {
-    console.error(`${path} is not a pnpm lock file`);
-    Deno.exit(1);
+  
+  print() {
+    this.packageNames.forEach(packageName => {
+      const size = this.fileSizeMap.get(packageName);
+      console.log(`${size || '    '}\t${packageName}`);
+    });
+    return this;
   }
-  const packages = Object.keys(parsed.packages).sort();
-  if (!onlyDuplicated) {
-    packages.forEach((packageName) => console.log(packageName));
-    Deno.exit(0);
+}
+
+const run = ({ path, onlyDuplicated, showFileSize }: RunArgs) => {
+  const runner = new Runner(path);
+  if (onlyDuplicated) {
+    runner.filterDuplicated();
   }
-  const result = new Set<string>();
-  for (let i = 0; i < packages.length - 1; i += 1) {
-    const cur = packages[i];
-    const next = packages[i + 1];
-    const curPackageName = cur.split("@")[cur.startsWith("/@") ? 1 : 0];
-    const nextPackageName = next.split("@")[next.startsWith("/@") ? 1 : 0];
-    if (curPackageName === nextPackageName) {
-      result.add(cur);
-      result.add(next);
-    }
+  if (showFileSize) {
+    runner.getFileSizes();
   }
-  result.forEach((packageName) => console.log(packageName));
+  runner.print();
 };
 
 command
